7a7c80a98badf875a6e9af248ef5f7f5b8250ef6
joshuaochia <joshuacosareochia@gmail.com>
2021-09-22 11:04:27 +0800

    fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,8 +4,6 @@ const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
