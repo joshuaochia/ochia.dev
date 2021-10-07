6e53b051dcea6757f6901f9962563f639fcb210b
joshuaochia <joshuacosareochia@gmail.com>
2021-10-07 11:32:28 +0800

    finish the responsive template website for about, blog, contact

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,13 @@
 // Jquery Import
 const $ = require("jquery");
 window.$ = $;
+
+// $(".progress-bar").animate(
+//   {
+//     width: "75%",
+//   },
+//   2500
+// );
+
+const bar = $(".progress-bar");
+console.log(bar);
